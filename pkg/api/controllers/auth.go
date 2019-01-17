43621dddd823619635725b983f2b72c6bf489acf
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-01-17 08:37:16 +0600

    remove - all old implementation
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- pkg/api/controllers/auth.go
+++
@@ -1,7 +0,0 @@
-package controllers
-
-import "net/http"
-
-func Login(w http.ResponseWriter, r *http.Request) {}
-func RefreshToken(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {}
-func Logout(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {}
